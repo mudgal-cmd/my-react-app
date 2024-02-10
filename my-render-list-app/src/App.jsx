4ae56cb2207894d5bd2d13f783c775b9f03b23f6
mudgal-cmd <nishant2020.mudgal@gmail.com>
2024-02-10 17:19:38 -0600

    Rendered List

--- my-render-list-app/src/App.jsx
+++ my-render-list-app/src/App.jsx
@@ -2,8 +2,33 @@ import List from "./List"
 
 function App() {
 
+  const fruits = [
+    {id:1, name: "Apple", calories: 95}, 
+    {id:2, name: "Orange", calories: 80}, 
+    {id:3, name: "Banana", calories: 100}, 
+    {id:4, name: "Coconut", calories: 125}, 
+    {id:5, name: "Pineapple", calories: 130}, 
+
+]
+
+const vegetables = [
+  {id:6, name: "Potatoes", calories: 35}, 
+  {id:7, name: "Cabbage", calories: 70}, 
+  {id:8, name: "Tomatoes", calories: 120}, 
+  {id:9, name: "Bell Pepper", calories: 45}, 
+  {id:10, name: "Carrots", calories: 110}, 
+
+]
+
   return(
-    <List></List>
+    <>
+      {/* Using the ternary operator */}
+      {/* {fruits.length > 0? <List items = {fruits} category ="Fruits"></List> : []} */} 
+      {/* Using the short circuit method, if left is false the remaining expression won't be evaluated */}
+      {fruits.length > 0 && <List items = {fruits} category ="Fruits"></List>}
+      {vegetables.length> 0? <List items = {vegetables} category = "Vegetables"></List>: []}
+    </>
+    
   );
 
 }
--- my-render-list-app/src/List.jsx
+++ my-render-list-app/src/List.jsx
@@ -1,42 +1,52 @@
-
-function List(){
+import PropTypes from "prop-types";
+function List(props){
   
-  const fruits = ['Apple', 'Orange', 'Banana', 'Coconut', 'Pineapple'];
+  // const fruits = ['Apple', 'Orange', 'Banana', 'Coconut', 'Pineapple'];
 
-  const fruitObj = [
-    {id:1, name: "Apple", calories: 95}, 
-    {id:2, name: "Orange", calories: 80}, 
-    {id:3, name: "Banana", calories: 100}, 
-    {id:4, name: "Coconut", calories: 125}, 
-    {id:5, name: "Pineapple", calories: 130}, 
-
-]
+  const itemsArray = props.items;
 
   // fruits.sort();
 
   // const fruitList = fruits.map(fruit => <li>{fruit}</li>);
 
-  fruitObj.sort((a,b) => a.name.localeCompare(b.name)); // Lexicographical or Alphabetical sorting acc to the fruit name
+  // fruitObj.sort((a,b) => a.name.localeCompare(b.name)); // Lexicographical or Alphabetical sorting acc to the fruit name
 
   // fruitObj.sort((a,b) => a.calories - b.calories); // sorting according to the calories / Numeric order / Asc
 
-  const fruitObjList = fruitObj.map(fruit => <li key ={fruit.id}>{fruit.name}: &nbsp; <strong>{fruit.calories}</strong></li>)
+  const itemObjList = itemsArray.map(item => <li className="list-item" key ={item.id}>{item.name}: &nbsp; <strong>{item.calories}</strong></li>)
 
-  const filteredFruits = fruitObj.filter(fruit => fruit.calories>100);
+  // const filteredFruits = fruitObj.filter(fruit => fruit.calories>100);
 
   return (
-    <ol>
+    <>
+    <h3 className="item-category">{props.category}</h3>
+      <ol className="item-list">
       {
       /* {fruitList} */
-
-        filteredFruits.map(fruit => <li key = {fruit.id}>{fruit.name}</li>)
+        // filteredFruits.map(fruit => <li key = {fruit.id}>{fruit.name}</li>)
+        itemObjList
 
       }
 
     </ol>
+    </>
     
   );
 
 }
+
+
+List.propTypes = {
+  
+  category: PropTypes.string,
+  items: PropTypes.arrayOf(PropTypes.shape({id: PropTypes.number, name: PropTypes.string, calories: PropTypes.number}))
+
+}
+
+List.defaultProps = {
+  category : "Category",
+  items: []
+}
+
 
 export default List
