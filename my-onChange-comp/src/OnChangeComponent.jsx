bba6eed2528f7af05f7d7db73a33c4cabcb1fbc5
mudgal-cmd <nishant2020.mudgal@gmail.com>
2024-02-19 21:53:21 -0600

    Added delivery type

--- my-onChange-comp/src/OnChangeComponent.jsx
+++ my-onChange-comp/src/OnChangeComponent.jsx
@@ -11,8 +11,20 @@ function OnChangeComponent(){
   const [payment, setPayment] = useState('VISA');
 
   const handlePaymentMethod = (event) => {
-    // setPayment(event.target.value);
     setPayment(event.target.value);
+  }
+
+  const [delivery, setDelivery] = useState('Delivery');
+
+  const handleDelivery = (event) =>{
+    setDelivery(event.target.value);
+    // console.log(event);
+  }
+
+  const [deliveryInst, setDeliveryInst] = useState('');
+
+  const handleDeliveryInstruction = (event) => {
+    setDeliveryInst(event.target.value);
   }
   
   return (
@@ -24,29 +36,11 @@ function OnChangeComponent(){
       <br/>
       <p>Do you want to add any delivery instructions? Please enter below:</p>
       <br/>
-      <textarea placeholder="Delivery Instructions" rows={10} cols={30}/>
+      <textarea placeholder="Delivery Instructions" rows={10} cols={30} onChange={(e)=> handleDeliveryInstruction(e)}/>
+      <br/>
+      <p>{deliveryInst}</p>
       <br/>
       <br/>
-      {/* <div>
-        <h4>Payment Method: </h4>
-        <label>
-        <input type="radio" value="VISA" name="payment-type" defaultChecked={true} onChange={(e) => handlePaymentMethod(e)}/>
-        VISA
-        </label>
-        <label>
-        <br/>
-        <br/>
-        <input type="radio" value="MASTERCARD" name="payment-type" onChange={(e) => handlePaymentMethod(e)}/>
-        MASTERCARD
-        </label>
-        <label>
-        <br/>
-        <br/>
-        <input type="radio" value="PAYPAL" name="payment-type" onChange={(e) => handlePaymentMethod(e)}/>
-        PAYPAL
-        </label>
-      </div>
-      <p>Opted Payment Method: {payment}</p> */}
       <select onChange={(e) => handlePaymentMethod(e)}>
         <option value="">Select Payment Method</option>
         <option value="VISA" selected={true}>VISA</option>
@@ -54,8 +48,29 @@ function OnChangeComponent(){
         <option value="AMEX">AMEX</option>
         <option value="PAYPAL">PAYPAL</option>
       </select>
-      <h4>Payment Method: {payment}</h4>
-     
+      <p>Payment Method: {payment}</p>
+
+      <div onChange={(e)=> handleDelivery(e)}>
+        <h4>Please select the delivery method</h4>
+        <label>
+          <input type="radio" name="delivery-method" value="Pick-Up"/>
+          &nbsp;&nbsp;Pick-up
+        </label>
+        <br/>
+        <br/>
+        <label>
+          <input type="radio" name="delivery-method" value="Delivery" defaultChecked="true"/>
+          &nbsp;&nbsp;Delivery
+        </label>
+        <br/>
+        <br/>
+        <label>
+          <input type="radio" name="delivery-method" value="Schedule Delivery"/>
+          &nbsp;&nbsp;Schedule Delivery
+        </label>
+        
+      </div>
+      <p>Opted Delivery method: {delivery}</p>
     </>
 
   );
